1fb0b110f8a5b9694008d13511c681f4d3cef4f1
Daniel Leong <me@dhleong.net>
2018-04-02 09:05:33 -0400

    Add `tools` module with a MigrationTester helper

---
+++ tools/src/androidTest/java/net/dhleong/sqlead/MigrationTester.kt
@@ -0,0 +1,94 @@
+package net.dhleong.sqlead
+
+import android.app.Instrumentation
+import android.arch.persistence.db.SupportSQLiteDatabase
+import android.arch.persistence.room.migration.Migration
+import android.arch.persistence.room.testing.MigrationTestHelper
+import android.content.Context
+import android.content.res.AssetManager
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doAnswer
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import java.io.File
+
+/**
+ * Convenient helper [TestRule] for running Room migration tests
+ *  without a real device.
+ *  
+ * @author dhleong
+ */
+class MigrationTester(
+    dbClass: Class<*>
+) : TestRule {
+
+    val assets = mock<AssetManager> {
+        on { open(any()) } doAnswer {
+            File("schemas", it.getArgument<String>(0))
+                .inputStream()
+        }
+    }
+
+    val context = mock<Context> {
+        on { assets } doReturn assets
+        on { getDatabasePath(any()) } doAnswer {
+            File(".", it.getArgument<String>(0)).also {
+                println(it.absoluteFile)
+                if (!it.parentFile.exists()) {
+                    it.parentFile.mkdirs()
+                }
+            }
+        }
+    }
+
+    val instrumentation = mock<Instrumentation> {
+        on { context } doReturn context
+        on { targetContext } doReturn context
+    }
+
+    val helper: MigrationTestHelper = MigrationTestHelper(
+        instrumentation,
+        dbClass.canonicalName,
+        SQLeadSQLiteOpenHelperFactory(inMemory = false)
+    )
+
+    override fun apply(base: Statement?, description: Description?): Statement =
+        helper.apply(base, description)
+
+    /**
+     * Execute a migration test
+     */
+    inline fun run(
+        migration: Migration,
+
+        /**
+         * Called with the old version of the database, so you
+         *  can insert some data to later test data persistence.
+         *  You cannot use DAO classes, since they expect the
+         *  latest schema.
+         */
+        withOld: (db: SupportSQLiteDatabase) -> Unit = { /* nop by default */ },
+
+        /**
+         * MigrationTestHelper automatically verifies schema changes,
+         *  but you should validate data migrations
+         */
+        withNew: (db: SupportSQLiteDatabase) -> Unit = { /* nop by default */ }
+    ) {
+        val name = "migration-test-${migration.startVersion}-${migration.endVersion}"
+
+        context.getDatabasePath(name).absoluteFile.delete()
+        helper.createDatabase(name, migration.startVersion)
+            .use(withOld)
+
+        helper.runMigrationsAndValidate(
+            name,
+            migration.endVersion,
+            true,
+            migration
+        ).use(withNew)
+    }
+}
